a832b63f7dc2247c6250edfb33bae9e8d8575d68
darklegend <bang@trustingsocial.com>
2018-02-08 23:12:29 +0700

    Update

--- lib.py
+++ lib.py
@@ -1,12 +1,21 @@
 import glob
 import os
+import random, string
 import random
 from config import *
 def getPhotos(mypath):
     return glob.glob(mypath + "/*.*")
 
 def getFullLink(photo):
-    return HOST + 'photo/'+ os.path.basename(photo)
+    folder = os.path.dirname(photo)
+    new_path = folder + '/' + getRandomName(20) + '.jpg'
+    path_local = os.rename(photo, new_path)
+    _link = HOST + 'photo/'+ os.path.basename(new_path)
+    return _link
+
+def getRandomName(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 def pickRandomly(photos):
     return random.choice(photos)
