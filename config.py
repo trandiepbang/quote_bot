68295d582200edcb7e1e11136cca5d9b67783f68
darklegend <bang@trustingsocial.com>
2018-02-24 21:39:26 +0700

    update

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 HOST = 'http://52.38.248.196:8000/'
 PHOTO_FOLDER = './quotes_photo'
 CHANNEL = '#batestbo'
+SENT_INTERVAL = 1
 BOT_USERNAME = 'Bang'
 WEBHOOK = 'https://hooks.slack.com/services/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'
--- quote_bot_schedule.py
+++ quote_bot_schedule.py
@@ -3,8 +3,9 @@ import time
 import redis
 import os
 from quote_bot import startJob
+from config import *
 
-schedule.every(0.5).minutes.do(startJob)
+schedule.every(SENT_INTERVAL).minutes.do(startJob)
 # schedule.every().day.at("22:30").do(reset)
 
 while True:
