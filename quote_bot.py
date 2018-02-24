38f599324f9d2ff59a26ac7b55fa549350674678
darklegend <bang@trustingsocial.com>
2018-02-24 21:31:02 +0700

    update

--- quote_bot.py
+++ quote_bot.py
@@ -12,8 +12,8 @@ def startJob():
         photos = getPhotos(PHOTO_FOLDER)
         selected_photo = pickRandomly(photos)
         FULL_LINK = getFullLink(selected_photo)
-        print(FULL_LINK)
-        # notifyChannel( { "text":FULL_LINK  , "channel": CHANNEL , "username": BOT_USERNAME } )
+        # print(FULL_LINK)
+        notifyChannel( { "text":FULL_LINK  , "channel": CHANNEL , "username": BOT_USERNAME } )
     except Exception , e:
         print("Something is wrong")
         print(e)
